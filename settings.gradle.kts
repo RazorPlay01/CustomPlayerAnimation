fa8afdce43cc7b663ddbb8db0634d1baf0bfba41
RazorPlay01 <82621554+RazorPlay01@users.noreply.github.com>
2025-11-24 10:25:48 -0500

    🌱 Changelog: Port to stonecutter - Minecraft version 1.21.8+ - Loaders: fabric, neoforge
    🐞 Known Bugs:
    -
    
    📋 Backlog:
    -

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,6 +24,7 @@ stonecutter {
 
 		match("1.21.8", "fabric", "neoforge")
 		match("1.21.9", "fabric", "neoforge")
+		match("1.21.10", "fabric", "neoforge")
 		//match("1.21.1", "fabric", "neoforge")
 
 		vcsVersion = "1.21.8-fabric"
