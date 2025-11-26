c2e1bf8171194160875f16fd13d8be2e10dc2159
RazorPlay01 <82621554+RazorPlay01@users.noreply.github.com>
2025-11-25 21:45:27 -0500

    🌱 Changelog: - Added config screen to neoforge
    🐞 Known Bugs:
    -
    
    📋 Backlog:
    -

--- src/main/java/com/github/razorplay01/cpa/platform/neoforge/NeoforgeEntrypoint.java
+++ src/main/java/com/github/razorplay01/cpa/platform/neoforge/NeoforgeEntrypoint.java
@@ -3,12 +3,23 @@ package com.github.razorplay01.cpa.platform.neoforge;
 //? neoforge {
 
 import com.github.razorplay01.cpa.ModTemplate;
+import com.github.razorplay01.cpa.platform.common.config.ConfigWrapper;
+import me.shedaniel.autoconfig.AutoConfig;
+import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+import java.util.function.Supplier;
 
 @Mod(ModTemplate.MOD_ID)
 public class NeoforgeEntrypoint {
 
 	public NeoforgeEntrypoint() {
+		ModLoadingContext.get().getActiveContainer().registerExtensionPoint(
+				IConfigScreenFactory.class,
+				(Supplier<IConfigScreenFactory>) () -> (minecraft, parentScreen) ->
+						AutoConfig.getConfigScreen(ConfigWrapper.class, parentScreen).get()
+		);
 		ModTemplate.onInitialize();
 	}
 }
