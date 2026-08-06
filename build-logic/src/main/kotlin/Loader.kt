8123da81b4cae425de2a5447f9296c7343af438e
RazorPlay01 <82621554+RazorPlay01@users.noreply.github.com>
2026-08-06 12:08:47 -0400

    fixed an error that prevented opening in-game configuration screens in fabric

--- build-logic/src/main/kotlin/Loader.kt
+++ build-logic/src/main/kotlin/Loader.kt
@@ -53,7 +53,8 @@ sealed class Loader(val id: String) {
 				entrypoints = mapOf(
 					"main" to listOf("${ctx.modGroup}.${ctx.modId}.platform.fabric.FabricEntrypoint"),
 					"client" to listOf("${ctx.modGroup}.${ctx.modId}.platform.fabric.FabricClientEntrypoint"),
-					"fabric-datagen" to listOf("${ctx.modGroup}.${ctx.modId}.platform.fabric.datagen.FabricDataGeneratorEntrypoint")
+					"fabric-datagen" to listOf("${ctx.modGroup}.${ctx.modId}.platform.fabric.datagen.FabricDataGeneratorEntrypoint"),
+					"modmenu" to listOf("com.github.razorplay01.cpa.platform.common.config.ModMenuConfig")
 				),
 				mixins = listOf("${ctx.modId}.mixins.json"),
 				depends = ctx.extension.dependencies.required.associate { it.modid.get() to it.fabricLikeVersionRange.get() },
